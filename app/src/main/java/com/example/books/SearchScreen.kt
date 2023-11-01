a1987dfa4d7839f60a9f5fe0f08c5751641d8ab5
LegendOfGrudge <nicholasfletcher3682@yahoo.ca>
2023-11-01 12:38:09 -0500

    Added book cover and title when searching using ISBN

--- app/src/main/java/com/example/books/SearchScreen.kt
+++ app/src/main/java/com/example/books/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.example.books
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,21 +26,44 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.google.android.engage.common.datamodel.Image
+import org.json.JSONArray
+import android.view.View
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.KeyboardType
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class,
+    ExperimentalComposeUiApi::class
+)
 @Composable
 fun SearchScreen(
     navController: NavController
 ){
+    val context = LocalContext.current;
+    val queue = Volley.newRequestQueue(context)
+    val focusManager = LocalFocusManager.current
     val padding = 16.dp
     var searchable by remember {
         mutableStateOf("")
     }
     var imageURL by remember {
         mutableStateOf("")
+    }
+    var bookTitle by remember {
+        mutableStateOf("")
+    }
+    var isSavedVisible by remember {
+        mutableStateOf(false)
     }
 
     Box(
@@ -53,19 +77,68 @@ fun SearchScreen(
                 modifier = Modifier.fillMaxWidth(),
                 value = searchable,
                 placeholder = { Text("ISBN...") },
-                onValueChange = { searchable = it }
+                onValueChange = { searchable = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
             Button(onClick = {
-                val url = "https://openlibrary.org/isbn/${searchable}.json"
-                imageURL = "https://en.m.wikipedia.org/wiki/File:Sample_abc.jpg"
+                focusManager.clearFocus()
+
+                val url = "https://openlibrary.org/isbn/${searchable.trim()}.json"
+
+                // Loading image
+                imageURL = "https://media1.giphy.com/media/6036p0cTnjUrNFpAlr/giphy.gif?cid=ecf05e479j2w1xbpa3tk0fx0b5mo6nax6c74nd8ct4mk6b64&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+
+                val text = "Failed to load image"
+                val duration = Toast.LENGTH_SHORT
+
+                val toast = Toast.makeText(context, text, duration) // in Activity
+
+                //Retrieves jsonObject
+                val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                    { response ->
+
+                        //Parse JSON
+                        //Gets an array from json in the key called "covers"
+                        val responseJSON: JSONArray = response.getJSONArray("covers")
+
+                        //Stores the cover ID
+                        val imageId: String = responseJSON.getString(0)
+                        imageURL = "https://covers.openlibrary.org/b/id/${imageId}-L.jpg"
+                        
+                        //Display book cover ID
+                        bookTitle = response.getString("title")
+
+                        isSavedVisible = true
+                    },//If an error occurs in fetching the data
+                    {
+                        bookTitle = "Failed to load image"
+                        isSavedVisible = false
+                        toast.show()
+                    })
+
+                // Add the request to the RequestQueue.
+                queue.add(jsonObjectRequest)
             }) {
                 Text("Search")
             }
-            GlideImage(
-                model = "https://en.m.wikipedia.org/wiki/File:Sample_abc.jpg",
-                contentDescription = "Book Cover",
-                modifier = Modifier.fillMaxSize()
-            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    GlideImage(
+                        model = imageURL,
+                        contentDescription = "Book Cover"
+                    )
+                    Text(text = bookTitle)
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.alpha(if (isSavedVisible) 1f else 0f)
+                    ) {
+                        Text("Save")
+                    }
+                }
+            }
         }
     }
 
